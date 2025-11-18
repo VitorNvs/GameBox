05eb211ce0de626f234295ad6a32a1915476af90
thyerri macedo <thyerri_macedo@hotmail.com>
2025-11-18 20:28:10 -0300

    bugs do mini perfil consertados

--- GameBox/models/User.js
+++ GameBox/models/User.js
@@ -5,12 +5,16 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role:     { type: String, default: 'user' }, // opcional: 'user' | 'admin'
+  role:     { type: String, default: 'user' },
   createdAt:{ type: Date, default: Date.now },
 
   avatar: { type: String, default: "" },
-  headerImg: { type: String, default: "" }
+  headerImg: { type: String, default: "" },
+
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 });
+
 
 // registra e exporta o model
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
